1c51613a0251013ad9c8457155e711a9b3c6c465
José Ribeiro <me@joseribeiro.dev>
2024-09-26 15:46:38 -0300

    chore: initial commit

---
+++ apps/govquests-frontend/lint-staged.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+
+/**
+ * This is the base lint-staged rules config and just includes prettier by default.
+ * A good practice is to override this base configuration in each package and/or application
+ * where we are able to add customization depending on the nature of the project (eslint...).
+ *
+ * {@link https://github.com/okonet/lint-staged#how-to-use-lint-staged-in-a-multi-package-monorepo}
+ * {@link https://github.com/belgattitude/nextjs-monorepo-example/blob/main/docs/about-lint-staged.md}
+ */
+
+const { concatFilesForPrettier } = require("../../lint-staged.common.js");
+
+/**
+ * @type {Record<string, (filenames: string[]) => string | string[] | Promise<string | string[]>>}
+ */
+const rules = {
+  "**/*.{css,scss,ts,js,tsx,jsx,mjs}": (filenames) => {
+    return [`pnpm biome check --write ${concatFilesForPrettier(filenames)}`];
+  },
+  "**/*.{json,md,mdx,html,yml,yaml}": (filenames) => {
+    return [`pnpm prettier --write ${concatFilesForPrettier(filenames)}`];
+  },
+};
+
+module.exports = rules;
