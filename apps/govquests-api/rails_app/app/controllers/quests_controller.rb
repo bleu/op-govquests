386385d1e9e9e64e690fa80e56ae414d46a4b9ba
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-10-01 16:59:21 -0300

    chore: add mocked quest detail

--- apps/govquests-api/rails_app/app/controllers/quests_controller.rb
+++ apps/govquests-api/rails_app/app/controllers/quests_controller.rb
@@ -137,4 +137,42 @@ class QuestsController < ApplicationController
 
     render json: quests
   end
+  def show
+    quest = {
+      img_url: "https://file.coinexstatic.com/2023-11-16/BB3FDB00283C55B4C36B94CFAC0C3271.png",
+      title: "Governance 101",
+      status: "start",
+      rewards: [
+        {
+          type: "Points",
+          amount: 50
+        },
+        {
+          type: "OP",
+          amount: 2
+        }
+      ],
+      intro: "First things first: understand what are the Optimism Values and what is expect of you in this important role.",
+      steps: [
+        {
+          content: "Code of Conduct",
+          duration: 15,
+        },
+        {
+          content: "Optimistic Vision",
+          duration: 10,
+        },
+        {
+          content: "Working Constitution",
+          duration: 15,
+        },
+        {
+          content: "Delegate Expectations",
+          duration: 12,
+        },
+      ]
+    }
+
+    render json: quest
+  end
 end
--- apps/govquests-api/rails_app/config/routes.rb
+++ apps/govquests-api/rails_app/config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'quests', to: 'quests#index'
+  get 'quests/:id', to: 'quests#show'
 end
