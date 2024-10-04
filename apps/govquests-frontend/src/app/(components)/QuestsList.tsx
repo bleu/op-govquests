659132d057eeef674a27123f86afcee31682dcec
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-10-04 12:59:38 -0300

    feat: update quest list and quest component

--- apps/govquests-frontend/src/app/(components)/QuestsList.tsx
+++ apps/govquests-frontend/src/app/(components)/QuestsList.tsx
@@ -1,28 +1,31 @@
 import Quest from "@/components/Quest";
 import type { Quest as IQuest, Reward } from "@/types/quest";
+import { MapIcon } from "lucide-react";
 import type React from "react";
 
 interface QuestsListProps {
-  quests: Pick<IQuest, "id" | "title" | "image_url" | "rewards" | "status">[];
+  quests: Pick<
+    IQuest,
+    "id" | "title" | "intro" | "image_url" | "rewards" | "status"
+  >[];
 }
 
 const QuestsList: React.FC<QuestsListProps> = ({ quests }) => {
   return (
-    <main className="p-6 flex-1">
-      <h2 className="text-2xl">Here's some quests for you!</h2>
-      <span className="text-lg bg-green mb-4">
-        We selected them based on your profile
-      </span>
-      <div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-5">
+    <main className="p-12 flex-1">
+      <div className="flex items-center">
+        <MapIcon width={30} height={30} />
+        <h2 className="text-3xl ml-3 font-medium">Our suggestions for you</h2>
+      </div>
+      <div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-7">
         {quests.map((quest) => (
           <Quest
             key={quest.id}
             id={quest.id}
             title={quest.title}
-            altText={quest.title}
+            intro={quest.intro}
             imageSrc={quest.image_url}
             rewards={quest.rewards}
-            status={quest.status}
           />
         ))}
       </div>
--- apps/govquests-frontend/src/components/Quest.tsx
+++ apps/govquests-frontend/src/components/Quest.tsx
@@ -1,4 +1,5 @@
 import type { Reward } from "@/types/quest";
+import { Map as MapIcon } from "lucide-react";
 import Link from "next/link";
 import type React from "react";
 import Image from "./Image";
@@ -6,49 +7,53 @@ import Image from "./Image";
 interface QuestProps {
   id: string;
   imageSrc: string;
-  altText: string;
   title: string;
+  intro: string;
   rewards: Reward[];
-  status: string;
 }
 
 const Quest: React.FC<QuestProps> = ({
   id,
   imageSrc,
-  altText,
   title,
+  intro,
   rewards,
-  status,
 }) => {
   return (
-    <div className="flex flex-col bg-red-300 rounded-lg">
-      <div className="relative w-full border border-black h-36">
-        <Image
-          src={imageSrc}
-          alt={altText}
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <div className="flex flex-col p-4">
-        <h3>{title}</h3>
-        <div className="flex gap-2">
-          {rewards.map((reward) => (
-            <span
-              key={reward.type}
-              className="bg-white self-start py-1 px-2 mt-1 mb-5 rounded text-sm"
-            >
-              {reward.amount} {reward.type}
-            </span>
-          ))}
+    <>
+      <Link
+        href={id}
+        className="flex flex-col bg-primary rounded-lg hover:bg-primary/70"
+      >
+        <div className="relative w-full h-40 rounded-t-lg overflow-hidden">
+          <div className="absolute right-3 top-3">
+            {rewards.map((reward) => (
+              <span
+                key={reward.type}
+                className="bg-optimism text-optimismForeground py-1 px-2 rounded-md text-sm ml-1"
+              >
+                {reward.amount} {reward.type}
+              </span>
+            ))}
+          </div>
+          <Image
+            src={
+              imageSrc ||
+              "https://file.coinexstatic.com/2023-11-16/BB3FDB00283C55B4C36B94CFAC0C3271.png"
+            }
+            alt={`Quest ${title} image`}
+            className="object-cover w-full h-full"
+          />
         </div>
-        <Link
-          href={id}
-          className="bg-white py-1 rounded-lg hover:bg-zinc-100 text-center"
-        >
-          {status}
-        </Link>
-      </div>
-    </div>
+        <div className="flex flex-col pl-6 pr-4 pb-6">
+          <div className="flex items-center gap-3 mt-2 mb-1 min-h-16">
+            <MapIcon width={25} height={25} />
+            <h3 className="font-bold text-lg line-clamp-2">{title}</h3>
+          </div>
+          <p className="text-md line-clamp-3">{intro}</p>
+        </div>
+      </Link>
+    </>
   );
 };
 
