ecdc55feb123d96336094ba45ccdc1219ead800e
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-10-07 15:52:34 -0300

    chore: add loading fallback

--- apps/govquests-frontend/src/app/quests/page.tsx
+++ apps/govquests-frontend/src/app/quests/page.tsx
@@ -1,10 +1,16 @@
 "use server";
 
 import api from "@/lib/api";
+import { Suspense } from "react";
 import QuestsList from "./(components)/QuestsList";
+import Loading from "./loading";
 
 export default async function Quests() {
   const quests = await api("quests");
 
-  return <QuestsList quests={quests} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <QuestsList quests={quests} />
+    </Suspense>
+  );
 }
