9be4d118c7e5d4df67f4d90133eb64c466efccc8
devjoaov <joaovictorass95@gmail.com>
2024-10-09 17:34:51 -0300

    chore: fix siwe

--- apps/govquests-api/rails_app/app/controllers/siwe_controller.rb
+++ apps/govquests-api/rails_app/app/controllers/siwe_controller.rb
@@ -1,21 +1,20 @@
 class SiweController < ApplicationController
   def nonce
-    render plain:SecureRandom.hex(32)
+    render plain: SecureRandom.hex(32)
   end
 
   def verify
-    binding.pry
-    begin
-      user = Authentication::UserReadModel.authenticate_with_siwe(params[:message], params[:signature])
-      token = generate_jwt(user)
-      render json: { token: token }
-    rescue Siwe::ExpiredMessage
-      render json: { error: 'Expired message' }, status: :unauthorized
-    rescue Siwe::NotValidMessage
-      render json: { error: 'Invalid message or signature' }, status: :unauthorized
-    rescue => e
-      render json: { error: e.message }, status: :bad_request
-    end
+    user = Authentication::UserReadModel.authenticate_with_siwe(params[:message], params[:signature])
+
+    # token = generate_jwt(user)
+    # render json: {token: token}
+    render json: {success: true}, status: :ok
+  rescue Siwe::ExpiredMessage
+    render json: {error: "Expired message"}, status: :unauthorized
+  rescue Siwe::NotValidMessage
+    render json: {error: "Invalid message or signature"}, status: :unauthorized
+  rescue => e
+    render json: {error: e.message}, status: :bad_request
   end
 
   private
@@ -23,7 +22,7 @@ class SiweController < ApplicationController
   def generate_jwt(user)
     payload = {
       user_id: user.user_id,
-      wallet_address: user.wallets.first['wallet_address'],
+      wallet_address: user.wallets.first["wallet_address"],
       exp: 24.hours.from_now.to_i
     }
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
--- apps/govquests-api/rails_app/app/read_models/authentication/read_model_configuration.rb
+++ apps/govquests-api/rails_app/app/read_models/authentication/read_model_configuration.rb
@@ -3,28 +3,39 @@ module Authentication
     self.table_name = "users"
 
     validates :user_id, presence: true, uniqueness: true
-    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_nil: true
-    validates :user_type, presence: true, inclusion: { in: %w[delegate non_delegate] }
-    
-    serialize :wallets, coder: JSON
+    validates :email, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}, allow_nil: true
+    validates :user_type, presence: true, inclusion: {in: %w[delegate non_delegate]}
+
+    # serialize :wallets, coder: JSON
 
     def self.find(address, chain_id)
-      find_by(wallets: [{ wallet_address: address, chain_id: chain_id }]) do |user|
-        user.user_id = SecureRandom.uuid
-        user.user_type = 'non_delegate'
-      end
+      where("wallets @> ?", [{wallet_address: address, chain_id: chain_id.to_i}].to_json).first
     end
 
     def self.authenticate_with_siwe(message, signature)
-      siwe_message = Siwe::Message.from_message(message)
-      if siwe_message.valid_signature?(signature)
-        user = find(siwe_message.address, siwe_message.chain_id)
-        user
-      else
-        nil
+      siwe_message = Siwe::Message.new(
+        message[:domain],
+        message[:address],
+        message[:uri],
+        message[:version],
+        {
+          statement: message[:statement],
+          issued_at: message[:issuedAt],
+          nonce: message[:nonce],
+          chain_id: message[:chainId].to_i,
+          expiration_time: "",
+          not_before: "",
+          request_id: "",
+          resources: []
+        }
+      )
+
+      if siwe_message.verify(signature, message[:domain], nil, message[:nonce])
+        find(siwe_message.address, siwe_message.chain_id)
       end
     end
   end
+
   class SessionReadModel < ApplicationRecord
     self.table_name = "user_sessions"
 
