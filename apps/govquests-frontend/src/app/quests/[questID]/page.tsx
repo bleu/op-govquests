9dc489167ed4f36830266100173c0191d0858a69
José Ribeiro <me@joseribeiro.dev>
2024-10-29 18:03:43 -0300

    chore: run next 15 update codemod

--- apps/govquests-frontend/src/app/quests/[questID]/page.tsx
+++ apps/govquests-frontend/src/app/quests/[questID]/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { use } from "react";
 
 import LoadingIndicator from "@/components/ui/LoadingIndicator";
 import { useFetchQuest } from "@/domains/questing/hooks/useFetchQuest";
 import QuestDetails from "@/domains/questing/components/QuestDetails";
 
 interface QuestDetailsPageProps {
-  params: {
+  params: Promise<{
     questID: string;
-  };
+  }>;
 }
 
-export default function QuestDetailsPage({
-  params: { questID },
-}: QuestDetailsPageProps) {
+export default function QuestDetailsPage(props: QuestDetailsPageProps) {
+  const params = use(props.params);
+
+  const {
+    questID
+  } = params;
+
   const { data, isLoading, isError } = useFetchQuest(questID as string);
 
   if (isLoading) {
