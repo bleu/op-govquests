a2a1a8043be55f31e8643394d6ed88e694d5b2c7
José Ribeiro <me@joseribeiro.dev>
2024-11-04 16:47:17 -0300

    chore: update dependencies

--- apps/govquests-api/govquests/processes/lib/processes.rb
+++ apps/govquests-api/govquests/processes/lib/processes.rb
@@ -4,7 +4,6 @@ require_relative "processes/start_quest_on_action_execution_started"
 require_relative "processes/update_profile_on_reward_issued"
 require_relative "processes/update_quest_progress_on_action_execution_completed"
 require_relative "processes/distribute_rewards_on_quest_completed"
-require_relative "processes/propose_token_claim_transaction"
 
 module Processes
   class Configuration
@@ -13,7 +12,6 @@ module Processes
       UpdateQuestProgressOnActionExecutionCompleted.new(event_store, command_bus).subscribe
       UpdateProfileOnRewardIssued.new(event_store, command_bus).subscribe
       DistributeRewardsOnQuestCompleted.new(event_store, command_bus).subscribe
-      ProposeTokenClaimTransaction.new(event_store, command_bus).subscribe
     end
   end
 end
