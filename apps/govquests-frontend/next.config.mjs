1d7714c054bcaed646ebe6f0a54fc1e516dfdde6
José Ribeiro <me@joseribeiro.dev>
2024-11-15 13:47:41 -0300

    chore: mark pino as external dep

--- apps/govquests-frontend/next.config.mjs
+++ apps/govquests-frontend/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ["pino", "pino-pretty"],
+  },
   async rewrites() {
     const destination =
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
