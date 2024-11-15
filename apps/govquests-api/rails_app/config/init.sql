0905bfe1dda320db0ab5909ab4f2971834220c0d
José Ribeiro <me@joseribeiro.dev>
2024-11-15 13:53:57 -0300

    feat: configure app to allow deploy using kamal

---
+++ apps/govquests-api/rails_app/config/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE govquests_api_production IF NOT EXISTS;
+CREATE DATABASE govquests_api_production_cache IF NOT EXISTS;
+CREATE DATABASE govquests_api_production_queue IF NOT EXISTS;
--- apps/govquests-api/rails_app/config/routes.rb
+++ apps/govquests-api/rails_app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount RailsEventStore::Browser => "/res"
   end
 
   post "/graphql", to: "graphql#execute"
-  mount RailsEventStore::Browser => "/res" if Rails.env.development?
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
