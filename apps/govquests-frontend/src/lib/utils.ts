231839194373ab8790ce1f4a31902fec372c7c70
José Ribeiro <me@joseribeiro.dev>
2024-11-15 13:59:01 -0300

    fix: prefix API_URL with window.location.origin on client

--- apps/govquests-frontend/src/lib/utils.ts
+++ apps/govquests-frontend/src/lib/utils.ts
@@ -10,5 +10,5 @@ export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/graphql"
     : isServer
-      ? process.env.NEXT_PUBLIC_API_URL
-      : "/graphql";
+      ? process.env.NEXT_PUBLIC_API_URL!
+      : window.location.origin + "/graphql";
