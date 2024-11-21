a3b8a9a817b83ee2122edb6945c03c261c2b0f36
José Ribeiro <me@joseribeiro.dev>
2024-11-21 15:31:35 -0300

    chore: fix typo

--- apps/govquests-api/rails_app/app/graphql/mutations/start_action_execution.rb
+++ apps/govquests-api/rails_app/app/graphql/mutations/start_action_execution.rb
@@ -39,7 +39,7 @@ module Mutations
         {
           address: current_user.address
         }
-      when "governance_vote_participation"
+      when "governance_voter_participation"
         {
           address: current_user.address
         }
