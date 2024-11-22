50d6f16ba2542f489c16700b3d1c8866439178f8
Juliano Nunes <julianonunes@Julianos-MacBook-Air.local>
2024-11-22 16:55:49 -0300

    feat: allow user to complete become_delegator action type

--- apps/govquests-api/govquests/action_tracking/lib/action_tracking/strategies/become_delegator.rb
+++ apps/govquests-api/govquests/action_tracking/lib/action_tracking/strategies/become_delegator.rb
@@ -1,12 +1,30 @@
 module ActionTracking
   module Strategies
     class BecomeDelegator < Base
+      include Import["services.agora"]
+
       def on_start_execution
-        
+        {
+          delegatees: 
+        }
       end
       
       def start_data_valid?
-        start_data[:address].present?
+        start_data[:address].present? && has_delegated?
+      end
+
+      def can_complete? 
+        start_data_valid?
+      end
+
+      private
+
+      def has_delegated?
+        delegatees.present?.any?
+      end
+
+      def delegatees 
+        @delegatees ||= agora.get_delegatees(start_data[:address])
       end
     end
   end
