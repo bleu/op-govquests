15bc5336400626e9625681e6ab81200c315413cb
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-02 09:38:48 -0400

    fix: remove token reward from all quests

--- apps/govquests-api/rails_app/db/seeds.rb
+++ apps/govquests-api/rails_app/db/seeds.rb
@@ -255,7 +255,7 @@ module QuestData
         requirements: "<span>To complete this quest, you need to have:</span> <ul><li>A <strong>ENS</strong> — if you don't have it, <a href='https://ens.domains/' target='_blank' rel='noopener noreferrer'>register your ENS here</a> and remember to <strong>choose a distinct username</strong> that represents you (like yourname.eth).</li> <li>A <strong>Discourse account</strong> on <strong>Optimism Governance Forum</strong> — if you also don't have it, <a href='https://gov.optimism.io/' target='_blank' rel='noopener noreferrer'>create your account here.</a> We recommend you to use your ENS as username so you can get easily recognizable.</li></ul>"
       },
       audience: "Delegates",
-      rewards: [{type: "Points", amount: 165}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}],
+      rewards: [{type: "Points", amount: 165}],
       actions: [ENS_ACTION, DISCOURSE_VERIFICATION_ACTION]
     },
     {
@@ -265,9 +265,7 @@ module QuestData
         image_url: "https://example.com/governance101.jpg"
       },
       audience: "AllUsers",
-      rewards: [
-        {type: "Points", amount: 55}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}
-      ],
+      rewards: [{type: "Points", amount: 55}],
       actions: UNLOCK_PROFILE_ACTIONS
     },
     {
@@ -277,7 +275,7 @@ module QuestData
         image_url: "https://example.com/governance101.jpg"
       },
       audience: "AllUsers",
-      rewards: [{type: "Points", amount: 165}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}],
+      rewards: [{type: "Points", amount: 165}],
       actions: READ_DOCUMENT_ACTIONS
     },
     {
@@ -288,7 +286,7 @@ module QuestData
         requirements: "Your Unique Humanity Score must be 20 or higher to complete this quest. Not there yet? Check some tips on how to increase your score."
       },
       audience: "Delegates",
-      rewards: [{type: "Points", amount: 55}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}],
+      rewards: [{type: "Points", amount: 55}],
       actions: [GITCOIN_ACTION]
     },
 
@@ -300,7 +298,7 @@ module QuestData
         requirements: "You need to be a delegate to do this quest! If you’re not one, start with Become Delegate Quest."
       },
       audience: "Delegates",
-      rewards: [{type: "Points", amount: 165}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}],
+      rewards: [{type: "Points", amount: 165}],
       actions: [VERIFY_POSITION_ACTION]
     },
     {
@@ -312,7 +310,7 @@ module QuestData
       },
       quest_type: "Governance",
       audience: "NonDelegates",
-      rewards: [{type: "Points", amount: 20}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}],
+      rewards: [{type: "Points", amount: 20}],
       actions: [VERIFY_DELEGATE_ACTION]
     },
     {
@@ -324,7 +322,7 @@ module QuestData
       },
       quest_type: "Governance",
       audience: "Delegates",
-      rewards: [{type: "Points", amount: 20}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}],
+      rewards: [{type: "Points", amount: 20}],
       actions: [VERIFY_DELEGATE_STATEMENT]
     },
     {
@@ -336,7 +334,7 @@ module QuestData
       },
       quest_type: "Governance",
       audience: "Delegates",
-      rewards: [{type: "Points", amount: 20}, {type: "Token", amount: 100, token_address: "0x4200000000000000000000000000000000000042", inventory: 100}],
+      rewards: [{type: "Points", amount: 20}],
       actions: [VERIFY_AGORA, VERIFY_FIRST_VOTE]
     },
     {
