fbe49c8af216d15e227da548b45d4796bdd7d584
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-03 13:59:52 -0400

    fix: enable font optimization

--- apps/govquests-frontend/src/app/layout.tsx
+++ apps/govquests-frontend/src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const jetBrains = JetBrains_Mono();
+const jetBrains = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
