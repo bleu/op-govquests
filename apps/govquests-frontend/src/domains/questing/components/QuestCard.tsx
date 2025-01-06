0f44399259698db1c7df76d880d0f5ca83d70264
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-06 17:57:04 -0400

    feat: add is completed state to quest card

--- apps/govquests-frontend/src/domains/questing/components/QuestCard.tsx
+++ apps/govquests-frontend/src/domains/questing/components/QuestCard.tsx
@@ -5,6 +5,7 @@ import RewardIndicator from "@/components/RewardIndicator";
 import Image from "@/components/ui/Image";
 import { Quests } from "../types/questTypes";
 import { IndicatorPill } from "@/components/IndicatorPill";
+import { cn } from "@/lib/utils";
 
 interface QuestProps {
   quest: Quests[number];
@@ -16,12 +17,19 @@ const QuestCard: React.FC<QuestProps> = ({ quest }) => {
   return (
     <Link href={`/quests/${quest.slug}`}>
       <div className="rounded-lg overflow-hidden group transition-all duration-300 hover:shadow-lg">
-        <div className="relative bg-secondary flex justify-center items-center p-6">
+        <div
+          className={cn(
+            "relative bg-secondary flex justify-center items-center p-6",
+            isCompleted && "bg-gradient-to-b from-[#80F2FF] to-[#A1E0D2]",
+          )}
+        >
           <div className="w-full text-center text-primary-foreground font-bold text-lg py-3 bg-primary border border-primary-foreground shadow-[1px_1px_0px_0px_#000000] rounded-lg transition-all duration-300 group-hover:shadow-[2px_2px_0px_0px_#000000] group-hover:scale-105">
             {quest.displayData.title}
           </div>
           <IndicatorPill className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2">
-            {quest.rewardPools[0].rewardDefinition.amount} points
+            {isCompleted
+              ? "Completed"
+              : quest.rewardPools[0].rewardDefinition.amount + " points"}
           </IndicatorPill>
         </div>
         <div className="bg-background/60 h-full p-7">
