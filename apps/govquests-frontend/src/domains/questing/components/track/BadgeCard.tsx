7dcceab11c256de97b7936b715b412336122aba9
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-07 08:41:50 -0400

    feat: add scale hover on badge image

--- apps/govquests-frontend/src/domains/questing/components/track/BadgeCard.tsx
+++ apps/govquests-frontend/src/domains/questing/components/track/BadgeCard.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { Badge } from "./TrackAccordion";
 import Image from "next/image";
+import Link from "next/link";
 
 interface BadgeCardProps {
   badge: Badge;
@@ -12,17 +13,19 @@ export const BadgeCard = ({ badge, isCompleted }: BadgeCardProps) => {
 
   return (
     <div className="relative items-center justify-center min-w-52 h-60 col-span-2">
-      <Image
-        src={"/badges/track1.png"}
-        alt="badge_image"
-        width={100}
-        height={100}
-        className={cn(
-          "object-cover w-full h-full grayscale",
-          isCompleted && "grayscale-0",
-        )}
-        unoptimized
-      />
+      <Link href={`/badges/${badge.id}`}>
+        <Image
+          src={"/badges/track1.png"}
+          alt="badge_image"
+          width={100}
+          height={100}
+          className={cn(
+            "object-cover w-full h-full grayscale hover:scale-105 transition-all duration-300",
+            isCompleted && "grayscale-0",
+          )}
+          unoptimized
+        />
+      </Link>
     </div>
   );
 };
