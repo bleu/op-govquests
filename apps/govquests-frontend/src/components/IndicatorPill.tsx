60e35d8f105e95d4e6e8272cd3e1674b60b8918e
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-10 09:36:31 -0300

    fix: fix indicator pill

--- apps/govquests-frontend/src/components/IndicatorPill.tsx
+++ apps/govquests-frontend/src/components/IndicatorPill.tsx
@@ -6,16 +6,12 @@ export const IndicatorPill = ({
   ...props
 }: ComponentProps<"span">) => {
   return (
-<<<<<<< HEAD
     <div
       className={cn(
         "relative border shadow-[1px_1px_0px_0px_#000000] bg-primary border-primary-foreground items-center text-center rounded-lg justify-center flex py-1 px-4",
         className,
       )}
     >
-=======
-    <div className="relative border shadow-[1px_1px_0px_0px_#000000] bg-primary border-primary-foreground items-center text-center rounded-lg justify-center flex py-1 px-4">
->>>>>>> ca009c899af116cb97a88482de138bdbf29c993f
       <AnchorPoint className="absolute top-1 left-1 " />
       <span
         className="text-primary-foreground text-xs font-bold whitespace-nowrap flex items-center"
