7e828990fbbc8a16d6f8dcceabd56681407f417e
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-13 15:34:25 -0300

    fix: scale down font when not completed

--- apps/govquests-frontend/src/domains/gamification/components/BadgeCard.tsx
+++ apps/govquests-frontend/src/domains/gamification/components/BadgeCard.tsx
@@ -29,11 +29,16 @@ export const BadgeCard = ({ badgeId, isCompleted }: BadgeCardProps) => {
             />
             <div
               className={cn(
-                "absolute left-1/2 -translate-x-1/2 group-hover:scale-105 flex h-full top-0 pb-[42px] items-end transition-all duration-300",
+                "absolute w-full right-0 group-hover:scale-105 flex h-full top-0 pb-[42px] items-end transition-all duration-300 whitespace-nowrap",
                 isCompleted ? koulen.className : redactedScript.className,
               )}
             >
-              <span className="text-primary-foreground translate-y-1/2 font-medium text-lg whitespace-nowrap">
+              <span
+                className={cn(
+                  "text-primary-foreground w-full text-center translate-y-1/2 tracking-tighter text-lg",
+                  !isCompleted && "scale-x-75",
+                )}
+              >
                 {data.badge.displayData.title}
               </span>
             </div>
