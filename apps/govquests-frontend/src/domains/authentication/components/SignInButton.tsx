2b4a07d032021fc0c59c93f918c95695994a1e23
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-14 15:28:22 -0300

    feat: add popover with account info

--- apps/govquests-frontend/src/domains/authentication/components/SignInButton.tsx
+++ apps/govquests-frontend/src/domains/authentication/components/SignInButton.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { Button } from "@/components/ui/Button";
+import { Popover } from "@/components/ui/popover";
 import Spinner from "@/components/ui/Spinner";
+import { cn } from "@/lib/utils";
+import {
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
 import { ConnectKitButton, useSIWE } from "connectkit";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, X } from "lucide-react";
 import React from "react";
-import { useAccount, useEnsAvatar, useEnsName } from "wagmi";
+import {
+  useAccount,
+  useBalance,
+  useEnsAvatar,
+  useEnsName,
+  useDisconnect,
+} from "wagmi";
 
 const WalletDisplay = () => {
   const { address } = useAccount();
@@ -33,6 +46,56 @@ const WalletDisplay = () => {
   );
 };
 
+const WalletPopover = ({ className }: { className: string }) => {
+  const { address } = useAccount();
+  const { data: ensName } = useEnsName({ address });
+  const { data: ensAvatar } = useEnsAvatar({ name: ensName });
+  const { disconnect } = useDisconnect();
+
+  const { data: balance } = useBalance({
+    address,
+  });
+
+  const getDisplayAddress = () => {
+    if (!address) return "";
+    if (ensName) return ensName;
+    return `${address.slice(0, 6)}...${address.slice(-4)}`;
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button variant="secondary" className={cn(className, "w-48")}>
+          <WalletDisplay />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="z-50 absolute -top-10 translate-x-1/2 right-10">
+        <div className="relative w-80 items-center text-center p-6 bg-background shadow-2xl rounded-xl min-h-fit flex flex-col gap-4">
+          <PopoverClose className="absolute top-2 right-4 size-4">
+            <X width={16} />
+          </PopoverClose>
+          <div className="size-16 rounded-full overflow-hidden">
+            <img
+              src={ensAvatar || `https://effigy.im/a/${address}`}
+              alt="wallet avatar"
+              className="size-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col">
+            <span className="font-extrabold text-lg">
+              {getDisplayAddress()}
+            </span>
+            <span className="font-medium text-sm opacity-60">
+              {Number(balance?.formatted).toFixed(4)} {balance?.symbol}
+            </span>
+          </div>
+          <Button onClick={() => disconnect()}>Disconnect</Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
 interface SignInButtonProps {
   className?: string;
 }
@@ -52,24 +115,27 @@ const SignInButton: React.FC<SignInButtonProps> = ({ className }) => {
 
   return (
     <ConnectKitButton.Custom>
-      {({ isConnecting, show }) => (
-        <Button
-          variant="default"
-          onClick={isConnected ? handleAuth : show}
-          disabled={isConnecting}
-          className={className}
-        >
-          {isConnecting ? (
-            <Spinner />
-          ) : !isConnected ? (
-            "Connect Wallet"
-          ) : !isSignedIn ? (
-            "Sign-In with Ethereum"
-          ) : (
-            <WalletDisplay />
-          )}
-        </Button>
-      )}
+      {({ isConnecting, show }) => {
+        if (isConnected && isSignedIn)
+          return <WalletPopover className={className} />;
+        else
+          return (
+            <Button
+              variant="default"
+              onClick={isConnected ? handleAuth : show}
+              disabled={isConnecting}
+              className={className}
+            >
+              {isConnecting ? (
+                <Spinner />
+              ) : !isConnected ? (
+                "Connect Wallet"
+              ) : (
+                !isSignedIn && "Sign-In with Ethereum"
+              )}
+            </Button>
+          );
+      }}
     </ConnectKitButton.Custom>
   );
 };
