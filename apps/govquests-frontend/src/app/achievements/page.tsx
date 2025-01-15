4361228036eae77aa62a8b1c10f9f14e998b5cf7
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-15 18:19:31 -0300

    feat: add gallery with all badges

--- apps/govquests-frontend/src/app/achievements/page.tsx
+++ apps/govquests-frontend/src/app/achievements/page.tsx
@@ -1,9 +1,11 @@
+import { SimpleBadgesSection } from "@/domains/gamification/components/GalleryBadgesSection";
 import { SpecialBadgesSection } from "@/domains/gamification/components/SpecialBadgesSection";
 
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col h-full items-center justify-center">
       <SpecialBadgesSection />
+      <SimpleBadgesSection />
     </div>
   );
 }
---
+++ apps/govquests-frontend/src/domains/gamification/components/GalleryBadgesSection.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { useSearchParams } from "next/navigation";
+import { useFetchBadges } from "../hooks/useFetchBadges";
+import { BadgeCard } from "./BadgeCard";
+import { BadgeDetails } from "./BadgeDetails";
+
+export const SimpleBadgesSection: React.FC = () => {
+  const params = useSearchParams();
+  const queryBadgeId = params.get("badgeId");
+
+  const { data } = useFetchBadges();
+
+  return (
+    data && (
+      <div className="flex flex-col items-center justify-center mx-8 w-[calc(100%-4rem)]">
+        <div className="flex flex-col gap-3 pb-4 border-b border-foreground/20 w-full">
+          <h1 className="text-2xl font-bold"># Gallery</h1>
+          <span className="text-xl">Discover all the badges you can earn.</span>
+        </div>
+
+        <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 w-full">
+          {data.badges.map((badge, index) => (
+            <Dialog defaultOpen={queryBadgeId == badge.id}>
+              <DialogTrigger>
+                <BadgeCard
+                  badgeId={badge.id}
+                  isCompleted={true}
+                  withTitle
+                  header={`BADGE #${index + 1}`}
+                  className="w-full"
+                />
+              </DialogTrigger>
+              <BadgeDetails badgeId={badge.id} />
+            </Dialog>
+          ))}
+        </div>
+      </div>
+    )
+  );
+};
