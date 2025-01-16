a1c0f7fb6f99732ccbd45757c5100250e96a6dc2
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-16 11:49:45 -0300

    feat: ordenate badges on query

--- apps/govquests-api/rails_app/app/graphql/resolvers/fetch_badges.rb
+++ apps/govquests-api/rails_app/app/graphql/resolvers/fetch_badges.rb
@@ -3,7 +3,21 @@ module Resolvers
     type [Types::BadgeType], null: true
 
     def resolve
-      Gamification::BadgeReadModel.all
+      current_user = context[:current_user]
+      badges = Gamification::BadgeReadModel.includes(:user_badges)
+
+      earned_badge_ids = Gamification::UserBadgeReadModel
+        .where(user_id: current_user.id)
+        .where(badgeable_type: "Gamification::BadgeReadModel")
+        .select(:badgeable_id)
+
+      return badges if earned_badge_ids.empty?
+
+      badges.order(
+        Arel.sql(
+          "CASE WHEN id = ANY(?) THEN 0 ELSE 1 END"
+        )
+      ).bind_values([earned_badge_ids])
     end
   end
 end
--- apps/govquests-api/rails_app/app/graphql/resolvers/fetch_special_badges.rb
+++ apps/govquests-api/rails_app/app/graphql/resolvers/fetch_special_badges.rb
@@ -3,7 +3,21 @@ module Resolvers
     type [Types::SpecialBadgeType], null: true
 
     def resolve
-      Gamification::SpecialBadgeReadModel.all
+      current_user = context[:current_user]
+      badges = Gamification::SpecialBadgeReadModel.includes(:user_badges)
+
+      earned_badge_ids = Gamification::UserBadgeReadModel
+        .where(user_id: current_user.id)
+        .where(badgeable_type: "Gamification::SpecialBadgeReadModel")
+        .select(:badgeable_id)
+
+      return badges if earned_badge_ids.empty?
+
+      badges.order(
+        Arel.sql(
+          "CASE WHEN id = ANY(?) THEN 0 ELSE 1 END"
+        )
+      ).bind_values([earned_badge_ids])
     end
   end
 end
