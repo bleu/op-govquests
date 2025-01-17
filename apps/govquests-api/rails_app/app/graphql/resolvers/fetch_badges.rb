12bf244adfcc6920e5c083d21fbf75de5da34396
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-17 08:39:54 -0300

    fix: fix badges ordering

--- apps/govquests-api/rails_app/app/graphql/resolvers/fetch_badges.rb
+++ apps/govquests-api/rails_app/app/graphql/resolvers/fetch_badges.rb
@@ -9,17 +9,17 @@ module Resolvers
       return badges unless current_user
 
       earned_badge_ids = Gamification::UserBadgeReadModel
-        .where(user_id: current_user.id)
+        .where(user_id: current_user.user_id)
         .where(badgeable_type: "Gamification::BadgeReadModel")
-        .select(:badgeable_id)
+        .pluck(:badgeable_id)
 
       return badges if earned_badge_ids.empty?
 
       badges.order(
         Arel.sql(
-          "CASE WHEN id = ANY(?) THEN 0 ELSE 1 END"
+          "CASE WHEN badges.id IN (#{earned_badge_ids.join(",")}) THEN 0 ELSE 1 END"
         )
-      ).bind_values([earned_badge_ids])
+      )
     end
   end
 end
