24c6d94070d5a5d2d6c23a4821d8585d82683554
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-22 16:25:42 -0300

    feat: add tools on header

--- apps/govquests-frontend/src/components/Header.tsx
+++ apps/govquests-frontend/src/components/Header.tsx
@@ -3,15 +3,56 @@
 import SignInButton from "@/domains/authentication/components/SignInButton";
 import { NotificationBell } from "@/domains/notifications/components/NotificationBell";
 import { cn, kdamThmorPro } from "@/lib/utils";
+import { Book, Calendar, Home, List, MessageCircle } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
 
 const headerPages = [
   { href: "/", label: "Home" },
   { href: "/quests", label: "Quests" },
   { href: "/achievements", label: "Achievements" },
   { href: "/leaderboard", label: "Leaderboard" },
+];
+
+const tools = [
+  {
+    icon: Home,
+    title: "Governance Forum",
+    description: "Join discussions",
+    href: "https://gov.optimism.io/",
+  },
+  {
+    icon: List,
+    title: "Agora",
+    description: "Explore proposals",
+    href: "https://agora.optimism.io/",
+  },
+  {
+    icon: Book,
+    title: "Gov Summarizer",
+    description: "View summaries",
+    href: "",
+  },
+  {
+    icon: MessageCircle,
+    title: "GovGPT",
+    description: "Ask questions",
+    href: "",
+  },
+  {
+    icon: Calendar,
+    title: "Governance Calendar",
+    description: "Check events",
+    href: "",
+  },
 ];
 
 const Header: React.FC = () => {
@@ -47,6 +88,33 @@ const Header: React.FC = () => {
                 </Link>
               </li>
             ))}
+
+            <NavigationMenu className="group relative">
+              <NavigationMenuList>
+                <NavigationMenuItem className="group hover:bg-background">
+                  <NavigationMenuTrigger className="flex items-center text-foreground/80 font-normal group-hover:font-extrabold px-3 py-1 text-md rounded-full transition-all duration-300 group-hover:scale-110">
+                    # Tools
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent className="hover:bg-background flex flex-col gap-4 p-4">
+                    {tools.map(({ icon: Icon, title, description, href }) => (
+                      <Link
+                        key={title}
+                        className="flex flex-col items-start gap-1 whitespace-nowrap"
+                        href={href}
+                      >
+                        <div className="flex items-center gap-2">
+                          <Icon className="size-3" />
+                          <h3 className="text-foreground text-sm">{title}</h3>
+                        </div>
+                        <p className="text-xs text-foreground/60">
+                          {description}
+                        </p>
+                      </Link>
+                    ))}
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
           </ul>
         </nav>
 
