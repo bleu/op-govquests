672f4cce4a99123669d4344fb451026e1fa61bc1
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-01-28 11:24:05 -0300

    feat: use multiplier to update game profile score

--- apps/govquests-api/rails_app/app/read_models/gamification/on_score_updated.rb
+++ apps/govquests-api/rails_app/app/read_models/gamification/on_score_updated.rb
@@ -5,7 +5,8 @@ module Gamification
       points = event.data[:points]
 
       game_profile = GameProfileReadModel.find_or_initialize_by(profile_id: profile_id)
-      game_profile.update!(score: game_profile.score + points)
+      points_x_multiplier = points * game_profile.tier.multiplier
+      game_profile.update!(score: game_profile.score + points_x_multiplier.to_i)
 
       leaderboard_id = game_profile.leaderboard.leaderboard_id
       Rails.configuration.command_bus.call(
