dc073f582b3ecfa40c0b34096d074d9c876afbc7
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-04 10:11:16 -0300

    refactor: order badges on frontend

--- apps/govquests-api/rails_app/app/graphql/resolvers/fetch_badges.rb
+++ apps/govquests-api/rails_app/app/graphql/resolvers/fetch_badges.rb
@@ -3,23 +3,7 @@ module Resolvers
     type [Types::BadgeType], null: true
 
     def resolve
-      current_user = context[:current_user]
-      badges = Gamification::BadgeReadModel.includes(:user_badges)
-
-      return badges unless current_user
-
-      earned_badge_ids = Gamification::UserBadgeReadModel
-        .where(user_id: current_user.user_id)
-        .where(badge_type: "Gamification::BadgeReadModel")
-        .pluck(:badge_id)
-
-      return badges if earned_badge_ids.empty?
-
-      badges.order(
-        Arel.sql(
-          "CASE WHEN badges.id IN (#{earned_badge_ids.join(",")}) THEN 0 ELSE 1 END"
-        )
-      )
+      Gamification::BadgeReadModel.all
     end
   end
 end
--- apps/govquests-api/rails_app/db/schema.rb
+++ apps/govquests-api/rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_01_20_205824) do
+ActiveRecord::Schema[8.1].define(version: 2025_02_03_185729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -189,6 +189,16 @@ ActiveRecord::Schema[8.1].define(version: 2025_01_20_205824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["badge_id"], name: "index_special_badges_on_badge_id", unique: true
+  end
+
+  create_table "track_quests", force: :cascade do |t|
+    t.string "track_id", null: false
+    t.string "quest_id", null: false
+    t.integer "position", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id", "position"], name: "index_track_quests_on_track_id_and_position", unique: true
+    t.index ["track_id", "quest_id"], name: "index_track_quests_on_track_id_and_quest_id", unique: true
   end
 
   create_table "tracks", force: :cascade do |t|
