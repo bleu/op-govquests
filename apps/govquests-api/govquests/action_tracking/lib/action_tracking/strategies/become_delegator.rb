70438522c19be82dc2bafb7c57a69d983f0e468f
José Ribeiro <me@joseribeiro.dev>
2025-02-04 13:08:11 -0300

    fix: become_delegator action strategy

--- apps/govquests-api/govquests/action_tracking/lib/action_tracking/strategies/become_delegator.rb
+++ apps/govquests-api/govquests/action_tracking/lib/action_tracking/strategies/become_delegator.rb
@@ -1,30 +1,31 @@
 module ActionTracking
   module Strategies
     class BecomeDelegator < Base
-      include Import["services.agora"]
+      include Import['services.agora']
 
       def on_start_execution
         {
-          delegatees: 
+          delegatees:
         }
       end
-      
+
       def start_data_valid?
         start_data[:address].present? && has_delegated?
       end
 
-      def can_complete? 
+      def can_complete?
         start_data_valid?
       end
 
       private
 
       def has_delegated?
-        delegatees.present?.any?
+        delegatees.any?
       end
 
-      def delegatees 
-        @delegatees ||= agora.get_delegatees(start_data[:address])
+      def delegatees
+        response = agora.get_delegatees(start_data[:address])
+        @delegatees ||= response[:delegatees]
       end
     end
   end
