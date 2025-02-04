f280fd4aaaf44fd65f6ca0d526453887d0ae4ddd
José Ribeiro <me@joseribeiro.dev>
2025-02-04 12:47:07 -0300

    fix: wrap tiers component with suspense

--- apps/govquests-frontend/src/app/leaderboard/page.tsx
+++ apps/govquests-frontend/src/app/leaderboard/page.tsx
@@ -1,13 +1,14 @@
 import { TierTabs } from "@/domains/gamification/components/TierTabs";
-import {
-  CurrentUserAchievementPanel,
-} from "@/domains/gamification/components/UserAchievementPanel";
+import { CurrentUserAchievementPanel } from "@/domains/gamification/components/UserAchievementPanel";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-9 p-8 mx-8">
       <CurrentUserAchievementPanel />
-      <TierTabs />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TierTabs />
+      </Suspense>
     </div>
   );
 }
