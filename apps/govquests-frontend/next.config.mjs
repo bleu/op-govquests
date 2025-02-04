0eb9063df06e21849f96e6dc0779d45b70de5137
José Ribeiro <me@joseribeiro.dev>
2025-02-04 12:46:50 -0300

    fix: remove pino-pretty warning

--- apps/govquests-frontend/next.config.mjs
+++ apps/govquests-frontend/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
   async rewrites() {
     const destination =
       process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/graphql";
