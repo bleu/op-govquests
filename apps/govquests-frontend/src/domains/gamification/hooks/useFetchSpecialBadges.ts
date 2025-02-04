8fe3760e6c7ec90cc7a73a8373ac3141890b29bb
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-04 15:33:29 -0300

    fix: move select from fetchSecialBadge to fetchSpecialBadges

--- apps/govquests-frontend/src/domains/gamification/hooks/useFetchSpecialBadges.ts
+++ apps/govquests-frontend/src/domains/gamification/hooks/useFetchSpecialBadges.ts
@@ -7,5 +7,11 @@ export const useFetchSpecialBadges = () => {
   return useQuery({
     queryKey: ["specialBadges"],
     queryFn: () => fetchAllSpecialBadges(),
+    select: (data) => {
+      data.specialBadges.sort(
+        (a, b) => Number(b.earnedByCurrentUser) - Number(a.earnedByCurrentUser),
+      );
+      return data;
+    },
   });
 };
--- apps/govquests-frontend/src/domains/gamification/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-export const sortBadges = (data) => {
-  data.badges.sort(
-    (a, b) => Number(b.earnedByCurrentUser) - Number(a.earnedByCurrentUser),
-  );
-  return data;
-};
