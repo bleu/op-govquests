f13df028941f74dab3c93f3d2db2305393ec8b88
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-05 07:57:14 -0300

    fix: align track indicator pills in a column

--- apps/govquests-frontend/src/domains/questing/components/track/TrackAccordionHeader.tsx
+++ apps/govquests-frontend/src/domains/questing/components/track/TrackAccordionHeader.tsx
@@ -11,9 +11,11 @@ export const TrackAccordionHeader = ({ track }: TrackAccordionHeader) => {
       <h1 className="text-2xl font-bold flex gap-2">
         <p className="text-foreground/60">#TRACK</p> {track.displayData.title}
       </h1>
-      <div className="flex gap-4 hover:no-underline">
-        <IndicatorPill>{track.quests.length} quests</IndicatorPill>
-        <IndicatorPill>
+      <div className="grid grid-cols-2 gap-4 w-64">
+        <IndicatorPill className="w-fit justify-self-end">
+          {track.quests.length} quests
+        </IndicatorPill>
+        <IndicatorPill className="w-fit justify-self-end">
           {track.isCompleted ? "Completed" : track.points + " points"}
         </IndicatorPill>
       </div>
