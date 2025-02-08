cd63a8c2727c3eaa64c6af5788bdef17bc24def9
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-08 10:45:36 -0300

    fix: update home page optimism sun

--- apps/govquests-frontend/src/app/page.tsx
+++ apps/govquests-frontend/src/app/page.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 export default async function Home() {
   return (
     <main className="flex flex-1 flex-col gap-8 h-full items-center justify-center text-center max-w-[600px] mx-auto">
-      <Image width={200} height={200} src="/optimismSun.svg" alt="opSun" />
+      <Image
+        width={500}
+        height={500}
+        src="/optimismSun.png"
+        alt="opSun"
+        className="size-48"
+      />
 
       <div className="flex flex-col gap-5">
         <h1 className="font-bold text-3xl [text-shadow:2px_4px_0_rgb(0,0,0)]">
