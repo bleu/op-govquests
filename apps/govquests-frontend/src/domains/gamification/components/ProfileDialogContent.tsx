94026436a048c0fb7f3046124bcaccad67727c7b
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-08 10:02:17 -0300

    fix: add fixed height to user dialog, and scroll to badge section

--- apps/govquests-frontend/src/domains/gamification/components/ProfileDialogContent.tsx
+++ apps/govquests-frontend/src/domains/gamification/components/ProfileDialogContent.tsx
@@ -9,7 +9,10 @@ import { UserBadgesCollection } from "./UserBadgesCollection";
 
 export const ProfileDialogContent = ({ userId }: { userId: string }) => {
   return (
-    <DialogContent className="p-0 sm:rounded-[20px] max-w-[800px] w-4/5">
+    <DialogContent
+      onOpenAutoFocus={(e) => e.preventDefault()}
+      className="p-0 rounded-[20px] sm:rounded-[20px] max-w-[800px] w-4/5"
+    >
       <DialogHeader>
         <DialogTitle hidden>User Information</DialogTitle>
         <DialogDescription asChild>
--- apps/govquests-frontend/src/domains/gamification/components/UserBadgesCollection.tsx
+++ apps/govquests-frontend/src/domains/gamification/components/UserBadgesCollection.tsx
@@ -4,7 +4,7 @@ import { SectionHeader } from "@/components/SectionHeader";
 import { useUserInfo } from "../hooks/useUserInfo";
 import { NormalBadgeCard, SpecialBadgeCard } from "./BadgeCard";
 import { cn } from "@/lib/utils";
-import { ComponentProps } from "react";
+import type { ComponentProps } from "react";
 
 interface UserBadgesCollectionProps extends ComponentProps<"div"> {
   userId: string;
@@ -18,43 +18,47 @@ export const UserBadgesCollection = ({
   const { data } = useUserInfo(userId);
 
   return (
-    data && (
-      <div className="flex flex-col p-7 text-foreground">
+    <div className="flex flex-col py-7 overflow-visible text-foreground">
+      <div className="px-7">
         <SectionHeader title="Badges Collection" />
-        <div
-          className={cn("grid md:grid-cols-4 grid-cols-3", className)}
-          {...props}
-        >
-          {data.user.userBadges.length ? (
-            data.user.userBadges.map(({ badge }) => {
-              if (badge.__typename == "Badge")
-                return (
-                  <NormalBadgeCard
-                    badgeId={badge.id}
-                    key={badge.id}
-                    withTitle={true}
-                    header={`${badge.badgeable.__typename} BADGE #${badge.displayData.sequenceNumber}`}
-                    revealIncomplete
-                  />
-                );
-              else
-                return (
-                  <SpecialBadgeCard
-                    badgeId={badge.id}
-                    key={badge.id}
-                    withTitle={true}
-                    header={`SPECIAL BADGE #${badge.displayData.sequenceNumber}`}
-                    revealIncomplete
-                  />
-                );
-            })
-          ) : (
-            <div className="w-full col-span-4 mt-4 text-foreground/80">
-              This user hasn't collected any badges yet.
-            </div>
-          )}
-        </div>
       </div>
-    )
+      <div
+        className={cn(
+          "grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 pt-4 h-64 overflow-y-scroll custom-scrollbar pr-4 pl-6 mr-1",
+          className,
+        )}
+        {...props}
+      >
+        {data?.user.userBadges.length ? (
+          data?.user.userBadges.map(({ badge }) => {
+            if (badge.__typename === "Badge")
+              return (
+                <NormalBadgeCard
+                  badgeId={badge.id}
+                  key={badge.id}
+                  withTitle={true}
+                  header={`${badge.badgeable.__typename} BADGE #${badge.displayData.sequenceNumber}`}
+                  revealIncomplete
+                  scaleDisabled
+                />
+              );
+            return (
+              <SpecialBadgeCard
+                badgeId={badge.id}
+                key={badge.id}
+                withTitle={true}
+                header={`SPECIAL BADGE #${badge.displayData.sequenceNumber}`}
+                revealIncomplete
+                scaleDisabled
+              />
+            );
+          })
+        ) : (
+          <div className="w-full col-span-4 mt-4 text-foreground/80">
+            This user hasn't collected any badges yet.
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
