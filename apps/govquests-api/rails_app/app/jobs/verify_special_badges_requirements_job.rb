d98e05b77d5f7e47daafbf4a2ecac7ee1024da97
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2025-02-11 16:43:40 -0300

    feat: improve check if user has badge on special badges job

--- apps/govquests-api/rails_app/app/jobs/verify_special_badges_requirements_job.rb
+++ apps/govquests-api/rails_app/app/jobs/verify_special_badges_requirements_job.rb
@@ -4,14 +4,14 @@ class VerifySpecialBadgesRequirementsJob < ApplicationJob
   def perform
     Authentication::UserReadModel.find_each do |user|
       Gamification::SpecialBadgeReadModel.find_each do |badge|
-        if !badge.user_badges.where(user_id: user.user_id).exists?
-          Rails.configuration.command_bus.call(
-            Gamification::UnlockSpecialBadge.new(
-              badge_id: badge.badge_id,
-              user_id: user.user_id
-            )
+        next if badge.user_badges.where(user_id: user.user_id).exists?
+
+        Rails.configuration.command_bus.call(
+          Gamification::UnlockSpecialBadge.new(s
+            badge_id: badge.badge_id,
+            user_id: user.user_id
           )
-        end
+        )
       end
     rescue => e
       Rails.logger.error "Failed to update voting power for user #{user.user_id}: #{e.message}"
